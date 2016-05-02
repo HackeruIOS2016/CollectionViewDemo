f20eea5f4df6d4d32d3b1879305a5a760dcfabcb
HackerU <Hackeru@Teacher.local>
2016-05-02 09:35:50 +0300

    finished with the controller for now moving to the story board

--- CollectionViewDemo/SeasonCollectionViewController.swift
+++ CollectionViewDemo/SeasonCollectionViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
 
 class SeasonCollectionViewController: UICollectionViewController {
 
@@ -17,9 +16,7 @@ class SeasonCollectionViewController: UICollectionViewController {
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
-//        // Register cell classes
-//        self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+ 
 
         // Do any additional setup after loading the view.
     }
@@ -43,17 +40,17 @@ class SeasonCollectionViewController: UICollectionViewController {
 
     override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return 12
     }
 
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath)
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("SeasonCell", forIndexPath: indexPath)
     
         // Configure the cell
     
